25e6a213583dee926a5f6639147f50a2822e6a56
shobekhan <shobekhan@yahoo.com>
2017-12-07 22:11:53 +0100

    minor changes

--- mail.php
+++ mail.php
@@ -1,38 +1,67 @@
 <?php
-// Multiple recipients
-$to = 'shobekhan@yahoo.com'; // note the comma
+require_once 'vendor/autoload.php';
 
-// Subject
+$to = 'shobekhan@yahoo.com';
 $subject = 'Email from website';
 
 $message = '
 <html>
 <head>
-  <title></title>
+    <title></title>
 </head>
 <body>
-  <p>Email from website</p>
-  <table>
-   ';
+<p>Email from website</p>
+<table>
+    ';
+
 foreach ($_POST as $key => $item) {
     $message .= '<tr><td>' . ucfirst($key) . '</td>';
-    $message .= '<td>' . $item . '</td></tr>';
+    if (is_array($item)) {
+        $message .= '<td>';
+        foreach ($item as $value) {
+            $message .= $value;
+        }
+        $message .= '</td></tr>';
+    } else {
+        $message .= '<td>' . $item . '</td></tr>';
+    }
 }
 
 $message .= '
-  </table>
+</table>
 </body>
 </html>';
 
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=UTF-8';
+$transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
 
-// Additional headers
-$headers[] = 'To: ' . $to;
-$headers[] = 'From: ' . $_POST['email'];
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
 
-// Mail it
-mail($to, $subject, $message, implode("\r\n", $headers));
+// Create a message
+$messageObj = (new Swift_Message('Email von webseite'))
+    ->setFrom($_POST['email'])
+    ->setTo([$to])
+    ->setBody($message);
 
-//echo $_FILES["file1"]["name"] . $message;
+$targetDir = "uploads/";
+if (isset($_FILES["file1"])) {
+    $fileName = basename($_FILES["file1"]["name"]);
+    $targetFile = __DIR__ . '/' . $targetDir . $fileName;
+    $fileUploadStatus = move_uploaded_file($_FILES["file1"]["tmp_name"], $targetFile);
+    //chmod($targetFile, 0777);
+    $messageObj->attach(Swift_Attachment::fromPath($targetFile));
+}
+if (isset($_FILES["file2"])) {
+    $fileName = basename($_FILES["file2"]["name"]);
+    $targetFile =  __DIR__ . '/' . $targetDir . $fileName;
+    $fileUploadStatus = move_uploaded_file($_FILES["file2"]["tmp_name"], $targetFile);
+    //chmod($targetFile, 0777);
+    $messageObj->attach(Swift_Attachment::fromPath($targetFile));
+}
+
+try {
+    $result = $mailer->send($messageObj);
+    echo 'done';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
